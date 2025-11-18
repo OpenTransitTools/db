5927506564315d1f4f01813c91976e797748c4ba
Alice Martin <vinsanau@gmail.com>
2025-11-17 18:04:39 -0800

    fix: start script

---
+++ scripts/start.sh
@@ -0,0 +1,25 @@
+#
+# restart geoserver and postgis
+#
+STDIR=`dirname $0`
+. $STDIR/base.sh
+
+PRUNE=NTRUE
+PG_LOG=pglog.out
+
+# re-start postgis
+cd $STDIR/../
+echo $PWD
+date
+
+docker compose down
+sleep 2
+
+if [ $PRUNE == "TRUE" ]; then
+  docker system prune -a -f
+  sleep 2
+fi
+
+docker compose up -d >> $PG_LOG 2>&1
+sleep 2
+cd -
