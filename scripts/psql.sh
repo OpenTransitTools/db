66ccbcfa6e536a71f8b982c1aab1ebdaf049bea4
Alice Martin <vinsanau@gmail.com>
2025-11-17 18:53:53 -0800

    fix: more cleanup

--- scripts/psql.sh
+++ scripts/psql.sh
@@ -2,14 +2,15 @@
 ## crete OTT spatial db for OTT
 ##
 DIR=`dirname $0`
-. $DIR/../base.sh
+. $DIR/base.sh
 
 sql_cmd=${1:-"INTERACTIVE"}
+u=${2:-$user}
 
 if [ "$sql_cmd" == "INTERACTIVE" ]; then
   cmd="$psql_term $db"
 else
-  cmd="$psql_ott -h 127.0.0.1 -c '$sql_cmd'"
+  cmd="$db_path/$u -h 127.0.0.1 -c '$sql_cmd'"
   echo $cmd
   eval $cmd
 fi
