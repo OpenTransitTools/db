387865c80f92a020ff1f670cf3c5b305ae0b267c
Alice <martiali@trimet.org>
2025-12-10 15:36:19 -0800

    updating env vars

--- source_env_file.sh
+++ source_env_file.sh
@@ -1,14 +1,17 @@
-# **note**: project name is *important* for networking between GeoServer and PostGIS
- export COMPOSE_PROJECT_NAME=kartozageoserver
- export POSTGRES_MAJOR_VERSION=17
- export POSTGIS_MAJOR_VERSION=3
- export POSTGIS_MINOR_RELEASE=5
- export # kartoza/postgis env variables https://github.com/kartoza/docker-postgis
- export POSTGRES_DB=gis
- export POSTGRES_USER=tmpublic
- export POSTGRES_PASS=tmpublic
- export ALLOW_IP_RANGE=0.0.0.0/0
- export POSTGRES_PORT=5432
- export POSTGRES_UID=724
- export POSTGRES_GID=724
- export RUN_AS_ROOT=false
+export COMPOSE_PROJECT_NAME=kartozageoserver
+export POSTGRES_MAJOR_VERSION=17
+export POSTGIS_MAJOR_VERSION=3
+export POSTGIS_MINOR_RELEASE=5
+export POSTGRES_DB=gis
+export POSTGRES_USER=tmpublic
+export POSTGRES_PASS=tmpublic
+export ALLOW_IP_RANGE=0.0.0.0/0
+export POSTGRES_PORT=5432
+export POSTGRES_UID=724
+export POSTGRES_GID=724
+export RUN_AS_ROOT=false
+export PGUSER=tmpublic
+export PGDBNAME=osm
+export PGPORT=5432
+export PGPROJ=2913
+export PGPASSWORD=tmpublic
