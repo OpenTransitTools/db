cf87d35a2a227cf2630174028706111222242fd4
Alice <martiali@trimet.org>
2025-12-10 10:29:25 -0800

    transition from pgsql2shp to ogr2ogr`

--- source_env_file.sh
+++ source_env_file.sh
@@ -1,42 +1,14 @@
-#!/bin/sh
-
-# Usage: ./load_env.sh path/to/your.env
-
-# Check if file was provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 path/to/.env"
-  exit 1
-fi
-
-ENV_FILE="$1"
-
-# Check if file exists and is readable
-if [ ! -f "$ENV_FILE" ] || [ ! -r "$ENV_FILE" ]; then
-  echo "Error: Cannot read file '$ENV_FILE'"
-  exit 1
-fi
-
-# Read and process each line
-while IFS= read -r line || [ -n "$line" ]; do
-  # Ignore empty lines and comments
-  case "$line" in
-    ''|\#*) continue ;;
-  esac
-
-  # Only handle lines in VAR=VALUE format
-  case "$line" in
-    [A-Za-z_]*=*)
-      var_name=$(printf "%s" "$line" | cut -d= -f1)
-      var_value=$(printf "%s" "$line" | cut -d= -f2-)
-      # Remove surrounding quotes from value
-      case "$var_value" in
-        \"*\"|\'*\')
-          var_value=$(printf "%s" "$var_value" | sed -e 's/^["'\'']//; s/["'\'']$//')
-          ;;
-      esac
-      echo "$var_name"
-      echo "$var_value"
-      export "$var_name=$var_value"
-      ;;
-  esac
-done < "$ENV_FILE"
+# **note**: project name is *important* for networking between GeoServer and PostGIS
+ export COMPOSE_PROJECT_NAME=kartozageoserver
+ export POSTGRES_MAJOR_VERSION=17
+ export POSTGIS_MAJOR_VERSION=3
+ export POSTGIS_MINOR_RELEASE=5
+ export # kartoza/postgis env variables https://github.com/kartoza/docker-postgis
+ export POSTGRES_DB=gis
+ export POSTGRES_USER=tmpublic
+ export POSTGRES_PASS=tmpublic
+ export ALLOW_IP_RANGE=0.0.0.0/0
+ export POSTGRES_PORT=5432
+ export POSTGRES_UID=724
+ export POSTGRES_GID=724
+ export RUN_AS_ROOT=false
