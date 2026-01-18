5ae128cd69f8f1bf83391152738bda08488f7dc3
Frank Purcell <fxpurcell@gmail.com>
2026-01-17 22:26:34 -0800

    fix(scripts): more cleanup to the scritps

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,8 +4,9 @@
 REDIR=`dirname $0`
 . $REDIR/base.sh
 
-DB_LOG=${1:-"log.out"}
-DOC_PRUNE=${2:-"FALSE"}
+NEW_DATA_DIR=${1:-"TRUE"}
+DOC_PRUNE=${2:-"TRUE"}
+DB_LOG=${3:-"log.out"}
 
 date
 
@@ -16,9 +17,15 @@ docker compose down
 sleep 2
 
 if [ $DOC_PRUNE == "TRUE" ]; then
-  docker system prune -a -f
+  cmd="docker system prune -a -f"
+  echo $cmd
+  eval $cmd
   sleep 2
 fi
 
-new_db_dir
+if [ $NEW_DATA_DIR == "TRUE" ]; then
+  echo "new data dir: rm -rf $PWD/new_db_dir"
+  new_db_dir
+fi
+
 docker compose up -d >> $DB_LOG 2>&1
