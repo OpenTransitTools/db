0124c00b96f89863bd2f01f8cc60a4e40a74eee3
Frank Purcell <fxpurcell@gmail.com>
2026-03-10 20:16:08 -0700

    fix(create): create schema, etc...

--- scripts/create.sh
+++ scripts/create.sh
@@ -4,14 +4,47 @@
 DIR=`dirname $0`
 . $DIR/base.sh
 
-## create user (default is user='ott' with pass='ott' -- change in ./base.sh)
-cmd="$psql -c \"CREATE USER ${user} WITH PASSWORD '${pass}';\""
-echo $cmd
-eval $cmd
+new_db=${1:-"$db"}
+new_schema=${2:-"$1"}
+new_user=${3:-"$user"}
+new_pass=${4:-"$pass"}
 
-# create ott DB (and formally osm $osm_db)
-for d in $db
-do
-  $psql -c "CREATE DATABASE ${d} WITH OWNER ${user};"
-  $psql $d -c "CREATE EXTENSION postgis;"
-done
+# create new user 
+if [ "$new_user" != " " ]; then
+  cmd="$psql -c \"CREATE USER ${new_user} WITH PASSWORD '${new_pass}';\""
+  echo $cmd
+  eval $cmd
+else
+  new_user="$user"
+fi
+
+# create new database
+if [ "$new_db" != " " ]; then
+  cmd="$psql -c \"DROP DATABASE ${new_db} WITH FORCE;\""
+  echo $cmd
+  eval $cmd
+
+  cmd="$psql -c \"CREATE DATABASE ${new_db} WITH OWNER ${user};\""
+  echo $cmd
+  eval $cmd
+
+  $psql $new_db -c "CREATE EXTENSION postgis;"
+else
+  new_db=$db
+fi
+
+
+# create new schema in that db
+if [ "$new_schema" != " " ]; then
+  cmd="$psql -c \"\c ${new_db}; DROP SCHEMA '${new_schema}' CASCADE;\""
+  echo $cmd
+  eval $cmd
+
+  cmd="$psql -c \"\c ${new_db}; CREATE SCHEMA ${new_schema};\""
+  echo $cmd
+  eval $cmd
+
+  cmd="$psql -c \"\c ${new_db}; GRANT ALL ON SCHEMA '${new_schema}' TO '${new_user}';\""
+  echo $cmd
+  eval $cmd
+fi
